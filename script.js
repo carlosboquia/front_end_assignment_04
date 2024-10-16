58649fcd823ba2525c7adf1f865555bac6ff1f5b
Carlos Boquia <cboquia@academic.rrc.ca>
2024-10-16 00:58:59 -0500

    updated validate form and added fave moment within error messages. also switched questions container to radio so that only one can be chosen. switched villains container to checkbox for multiple selections

--- script.js
+++ script.js
@@ -1,24 +1,22 @@
-const nameContainerNode = document.querySelector(".nameContainer")
-const questionsContainerNode = document.querySelector(".questionsContainer")
-const villainContainerNode = document.querySelector(".villainContainer")
-const sidekickContainerNode = document.querySelector(".sidekickContainer")
-const faveMomentNodeContaineNode = document.querySelector(".fave-moment-container")
-const emailContainerNode = document.querySelector(".emailContainer")
-const inputContainerNode = document.querySelector(".inputContainer")
+const formNode = document.querySelector("#form")
 
 
 formNode.addEventListener("submit", (event) => {
+    // prevents form from going through when clicking "submit"
     event.preventDefault();
 
+    // clears error messages when inputted is correct
     const errorMessageNodes = document.querySelectorAll(".error-message");
     errorMessageNodes.forEach(node => {
         node.remove();
     });
     
+
+    
     const validateObject = validateForm();
 
 
-    if(!validateObject["isValid"]) {
+    if (!validateObject["isValid"]) {
         const errorMessages = validateObject["errorMessages"];
         errorMessages.forEach(message => {
             displayErrorMessage(formNode, message);
@@ -28,7 +26,50 @@ formNode.addEventListener("submit", (event) => {
 
 
 
-function validateform() {
+const validateForm = () => {
+    const emailContainerNode = document.querySelector("#email")
+    const nameContainerNode = document.querySelector("#name")
+
+    let isValid = true;
+    let errorMessages = [];
+
+    // name input error message
+    let inputName = nameContainerNode.value;
+    if(inputName.length <= 0) {
+        isValid = false;
+        errorMessages.push("Name cannot be empty.");
+    }
+
+    // email input error message
+    const emailInputValue = emailContainerNode.value;
+    const emailPattern = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,63}$/i;
+
+    if(!emailPattern.test(emailInputValue)) {
+        isValid = false;
+        errorMessages.push("Please enter a valid email address.")
+    }
+
+    // Fave moment input error message
+    const inputTextNode = document.querySelector("#fave-moment").value;
+    if (inputTextNode === "") {
+        isValid = false;
+        errorMessages.push("Favorite moment must be filled out")
+    }
+
+    return {
+        isValid: isValid,
+        errorMessages: errorMessages
+    };
+};
+
+const displayErrorMessage = (displaynode, errorMessage) => {
+    const errorTextNode = document.createElement("div");
+
+    errorTextNode.setAttribute("role", "alert");
+    errorTextNode.innerText = errorMessage;
+    errorTextNode.className = "error-message";
+
+    displaynode.appendChild(errorTextNode);
+};
 
 
-}
